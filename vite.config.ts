326e3baf73d2ee21b81c8961bb673c8875376e00
RehanDias <rehanpratama282@gmail.com>
2025-05-04 23:05:51 +0700

    update for github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tetris/',
+  base: '/tetris-game/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
